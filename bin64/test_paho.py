959d9161ed4e8d3c58ab981f60db3e5d158222b9
gpagani <g.pagani.privato@gmail.com>
2023-05-10 14:55:42 +0200

    v 1.64 - correzione publish mqtt dispositivi "generici" (type 11 e 12)

--- bin64/test_paho.py
+++ bin64/test_paho.py
@@ -10,7 +10,7 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("#")
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(msg.topic+" : "+str(msg.payload))
 
 client = mqtt.Client()
 client.on_connect = on_connect
